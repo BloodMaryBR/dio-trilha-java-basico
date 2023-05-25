47288c047a296f02b08fefdea1292254fee570a2
Lucas Oliveira <lucas_samuel@live.com>
2023-05-25 16:22:22 +0000

    implementação do codigo para nome,agencia,nconta

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      //todo: conhecer e importar a classe scanner
+      // exibir as mensagens para usuario
+      //obter pela classe scanner os valores digitados no terminal
+      // exibir a msg da conta criada
+
+
+System.out.println("Olá Cliente. Digite  seu nome:");
+Scanner entrada = new Scanner(System.in);
+String cliente = entrada.nextLine();
+
+      System.out.printf("Olá %s.Digite a sua agencia: ", cliente);
+      
+      String agencia = entrada.nextLine();
+      System.out.printf("A agencia digitada foi: %s", agencia);
+
+      System.out.printf("Agencia digitada foi: %s digite o numero da conta: " , agen);
+
+        
+
+      entrada.close();
     }
 }
